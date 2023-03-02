7c5f2d895326f8fd0373a45f13a929280ad50f5f
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-03-02 19:44:12 -0300

    Added gitignore and lesson 2 of course 1

--- spring-boot-essentials-1/devdojo-springboot/src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
+++ spring-boot-essentials-1/devdojo-springboot/src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
@@ -1,11 +1,13 @@
 package br.com.devdojo.awesome.endpoint;
 
 import br.com.devdojo.awesome.model.Student;
-import org.springframework.context.annotation.ComponentScan;
+import br.com.devdojo.awesome.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,8 +15,12 @@ import java.util.List;
 @RequestMapping("student")
 public class StudentEndpoint {
 
+    @Autowired
+    private DateUtil dateUtil;
+
     @GetMapping("/list")
     public List<Student> listAll() {
+        System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return Arrays.asList(new Student("João"), new Student("Maria"));
     }
 
