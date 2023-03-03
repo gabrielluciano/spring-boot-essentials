3eefeaadb67c3df9f895bd432492a367b0236aa8
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-03-02 22:45:00 -0300

    lessons 05, 06 and 07 (course 1)

--- spring-boot-essentials-1/springboot-esssentials/src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
+++ spring-boot-essentials-1/springboot-esssentials/src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
@@ -1,27 +1,69 @@
 package br.com.devdojo.endpoint;
 
+import br.com.devdojo.error.CustomErrorType;
 import br.com.devdojo.model.Student;
 import br.com.devdojo.util.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("student")
+@RequestMapping("students")
 public class StudentEndpoint {
 
-    @Autowired
     private DateUtil dateUtil;
 
-    @GetMapping("/list")
-    public List<Student> listAll() {
-        System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return Arrays.asList(new Student("João"), new Student("Maria"));
+    @Autowired
+    public StudentEndpoint(DateUtil dateUtil) {
+        this.dateUtil = dateUtil;
     }
 
+    @GetMapping
+    public ResponseEntity<?> listAll() {
+//        System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getStudentById(@PathVariable("id") int id) {
+        Optional<Student> studentOptional = Student.studentList.stream()
+                .filter(student -> student.getId() == id)
+                .findFirst();
+
+        return studentOptional.isPresent() ?
+                new ResponseEntity<>(studentOptional.get(), HttpStatus.OK) :
+                new ResponseEntity<>(new CustomErrorType("Student not found"),
+                        HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody Student student) {
+        Student.studentList.add(student);
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> delete(@RequestBody Student student) {
+        boolean deleted = Student.studentList.remove(student);
+        return new ResponseEntity<>(deleted ? HttpStatus.NO_CONTENT : HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(@RequestBody Student student) {
+        Student.studentList.remove(student);
+        Student.studentList.add(student);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
+
--- spring-boot-essentials-1/springboot-esssentials/src/main/java/br/com/devdojo/model/Student.java
+++ spring-boot-essentials-1/springboot-esssentials/src/main/java/br/com/devdojo/model/Student.java
@@ -1,8 +1,24 @@
 package br.com.devdojo.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class Student {
 
+    private int id;
     private String name;
+    public static List<Student> studentList;
+
+    static {
+        studentRepository();
+    }
+
+    public Student(int id, String name) {
+        this(name);
+        this.id = id;
+    }
 
     public Student(String name) {
         this.name = name;
@@ -11,11 +27,38 @@ public class Student {
     public Student() {
     }
 
+    private static void studentRepository() {
+        studentList = new ArrayList<>(Arrays.asList(
+                new Student(1, "João"), new Student(2, "Maria")
+        ));
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
