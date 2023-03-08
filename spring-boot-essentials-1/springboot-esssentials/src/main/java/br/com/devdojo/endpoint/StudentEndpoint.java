6426d5ec37731257136667588f482bc1ece8353b
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-03-08 20:07:35 -0300

    lesson 17 (course 1)

--- spring-boot-essentials-1/springboot-esssentials/src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
+++ spring-boot-essentials-1/springboot-esssentials/src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,7 +39,9 @@ public class StudentEndpoint {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getStudentById(@PathVariable("id") Long id) {
+    public ResponseEntity<?> getStudentById(@PathVariable("id") Long id,
+                                            @AuthenticationPrincipal UserDetails userDetails) {
+        System.out.println(userDetails);
         verifyIfStudentExists(id);
         return new ResponseEntity<>(studentDAO.findById(id).get(), HttpStatus.OK);
     }
@@ -53,6 +58,7 @@ public class StudentEndpoint {
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         verifyIfStudentExists(id);
         studentDAO.deleteById(id);
