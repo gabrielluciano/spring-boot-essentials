fc17000f1355c4ddd282fd854ab1f337a152c285
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-03-22 18:11:21 -0300

    lessons 10 to 12 (course 2)

--- spring-boot-essentials-2/springboot-essentials/src/main/java/academy/devdojo/springbootessentials/controller/AnimeController.java
+++ spring-boot-essentials-2/springboot-essentials/src/main/java/academy/devdojo/springbootessentials/controller/AnimeController.java
@@ -7,9 +7,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,5 +42,17 @@ public class AnimeController {
     @PostMapping
     public ResponseEntity<Anime> save(@RequestBody Anime anime) {
         return new ResponseEntity<>(animeService.save(anime),  HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> replace(@RequestBody Anime anime) {
+        animeService.replace(anime);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id) {
+        animeService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
