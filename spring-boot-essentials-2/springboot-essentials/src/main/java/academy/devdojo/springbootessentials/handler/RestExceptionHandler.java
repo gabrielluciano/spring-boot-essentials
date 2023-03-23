921f5d3959aa821f1487d60de555600471f4dad8
Gabriel Luciano <gabriel-luciano@outlook.com>
2023-03-23 19:59:52 -0300

    lesson 22 (course 2)

--- spring-boot-essentials-2/springboot-essentials/src/main/java/academy/devdojo/springbootessentials/handler/RestExceptionHandler.java
+++ spring-boot-essentials-2/springboot-essentials/src/main/java/academy/devdojo/springbootessentials/handler/RestExceptionHandler.java
@@ -2,20 +2,24 @@ package academy.devdojo.springbootessentials.handler;
 
 import academy.devdojo.springbootessentials.exception.BadRequestException;
 import academy.devdojo.springbootessentials.exception.BadRequestExceptionDetails;
+import academy.devdojo.springbootessentials.exception.ExceptionDetails;
 import academy.devdojo.springbootessentials.exception.ValidationExceptionDetails;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<BadRequestExceptionDetails> handleBadRequestException(BadRequestException ex) {
@@ -24,15 +28,15 @@ public class RestExceptionHandler {
                 .status(HttpStatus.BAD_REQUEST.value())
                 .title("Bad Request Exception, Check the Documentation")
                 .details(ex.getMessage())
-                .developerMessage(ex.getClass().getName())
+                .developerMessage(ex.getClass().getSimpleName())
                 .build();
 
         return new ResponseEntity<>(badRequestExceptionDetails, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ValidationExceptionDetails> handleMethodArgumentNotValidException(
-            MethodArgumentNotValidException ex) {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
         List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
 
@@ -52,4 +56,18 @@ public class RestExceptionHandler {
         return new ResponseEntity<>(validationExceptionDetails, HttpStatus.BAD_REQUEST);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+                                                             HttpStatus status, WebRequest request) {
+
+        var exceptionDetails = ExceptionDetails.builder()
+                .timestamp(LocalDateTime.now())
+                .status(status.value())
+                .title(ex.getCause().getMessage())
+                .details(ex.getMessage())
+                .developerMessage(ex.getClass().getSimpleName())
+                .build();
+
+        return new ResponseEntity<>(exceptionDetails, headers, status);
+    }
 }
